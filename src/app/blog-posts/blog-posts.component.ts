0b6a5fde15d5d43ae1218f154fa4e5dfba59f885
Igor Gursky <igor.gursky@ctco.lv>
2025-05-27 13:33:20 +0300

    blogposts: hooked up to a productive endpoint

--- src/app/blog-posts/blog-posts.component.ts
+++ src/app/blog-posts/blog-posts.component.ts
@@ -2,6 +2,7 @@ import {Component, inject} from '@angular/core';
 import {BlogPostsService} from "./blog-posts.service";
 import {BlogPostsContainerComponent} from "./blog-posts-container/blog-posts-container.component";
 import {IBlogPost} from "./blog-post.model";
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-blog-posts',
@@ -13,12 +14,18 @@ import {IBlogPost} from "./blog-post.model";
 })
 export class BlogPostsComponent {
   private blogPostsService: BlogPostsService = inject(BlogPostsService);
-
+  private snackbar = inject(MatSnackBar);
   blogPosts: IBlogPost[] = [];
 
   constructor() {
-    this.blogPostsService.getBlogPosts().subscribe(blogPosts => {
-      this.blogPosts = blogPosts;
-    });
+    this.blogPostsService
+      .getBlogPosts()
+      .subscribe({
+          next: (blogPosts) => this.blogPosts = blogPosts,
+          error: (err) => this.snackbar.open(err.message, 'OK', {
+            duration: 5000
+          })
+        }
+      );
   }
 }
