3f6df8b02b1ac2a62fbcfcfed22d383960e8f963
Ievgien <ievgien.iaroshenko@ctco.lv>
2025-05-27 16:49:16 +0400

    add comments

--- src/app/blog-comment/blog-comment.component.ts
+++ src/app/blog-comment/blog-comment.component.ts
@@ -5,7 +5,7 @@ import { ActivatedRoute } from '@angular/router';
 import { BlogService } from './blog.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { switchMap } from 'rxjs';
+import { startWith, Subject, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-blog-comment',
@@ -14,11 +14,18 @@ import { switchMap } from 'rxjs';
   styleUrl: './blog-comment.component.scss'
 })
 export class BlogCommentComponent{
-  blogId!: number;
+  blogId!: number; // Assuming blogId is set somewhere in the component
   blog$ = this.route.paramMap.pipe(
     switchMap(params => this.blogService.getBlog(Number(params.get('id'))))
   );
-  comments: IComment[] = [];
+  private refreshComments$ = new Subject<void>();
+
+comments$ = this.refreshComments$.pipe(
+  startWith(null), // so it loads initially
+  switchMap(() => this.route.paramMap.pipe(
+    switchMap(params => this.blogService.getComments(Number(params.get('id'))))
+  ))
+);
   newComment: IComment = {
     id: 0,
     name: '',
@@ -27,13 +34,23 @@ export class BlogCommentComponent{
     date: new Date()
   };
   constructor(private route: ActivatedRoute, private blogService: BlogService) {
+      this.route.paramMap.subscribe(params => {
+    this.blogId = Number(params.get('id'));
+    // Now you can use blogId in your component
+    console.log('Blog ID from URL:', this.blogId);
+  });
     }
 
   addComment() {
     if (this.newComment.name && this.newComment.email && this.newComment.message) {
-      this.newComment.id = this.comments.length + 1; // Simple ID generation
       this.newComment.date = new Date();
-      this.comments.push({ ...this.newComment });
+      console.log('blog Id to sed:', this.blogId);
+      this.blogService.postComment(this.blogId, this.newComment).subscribe(comment => {
+        // Optionally, you can handle the response here
+        console.log('Comment added:', comment);
+      });
+        // Refresh the comments list
+    this.refreshComments$.next();
       this.newComment = { id: 0, name: '', email: '', message: '', date: new Date() }; // Reset form
     } else {
       alert('Please fill in all fields.');
--- src/app/blog-comment/blog.service.ts
+++ src/app/blog-comment/blog.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IBlog } from './blog.model';
 import { Observable } from 'rxjs';
+import { IComment } from './comment.model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,5 +12,12 @@ export class BlogService {
   constructor(private http: HttpClient) { }
   getBlog(id: number): Observable<IBlog> {
     return this.http.get<IBlog>(`/api/blogs/${id}`);
+  }
+  getComments(blogId: number): Observable<IComment[]> {
+    return this.http.get<IComment[]>(`/api/blogs/${blogId}/comments`);
+  }
+  postComment(blogId: number, comment: IComment): Observable<IComment> {
+    console.log('Posting comment:', comment);
+    return this.http.post<IComment>(`/api/blogs/${blogId}/comments`, comment);
   }
 }
