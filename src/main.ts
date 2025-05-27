62662828f986bd2b5b4b2d01548256f19a7636d1
Igor Gursky <igor.gursky@ctco.lv>
2025-05-27 18:03:40 +0300

    other: attempts to setup router store

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
+import {AppComponent} from './app/app.component';
 import {bootstrapApplication} from '@angular/platform-browser';
 import {provideRouter} from '@angular/router';
 import {provideHttpClient} from '@angular/common/http';
+import {provideStore} from "@ngrx/store";
+import {provideRouterStore, routerReducer} from "@ngrx/router-store";
 import {routes} from './app/routes';
-import {AppComponent} from './app/app.component';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideStore({
+      router: routerReducer,
+    }),
+    provideRouterStore(),
   ]
 })
   .catch(err => console.error(err));
