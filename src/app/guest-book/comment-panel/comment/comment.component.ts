7f981815a1cc2b41a9a96b984ada1060a5e4eca8
Ievgien <ievgien.iaroshenko@ctco.lv>
2025-05-28 19:47:10 +0400

    change to signal

--- src/app/guest-book/comment-panel/comment/comment.component.ts
+++ src/app/guest-book/comment-panel/comment/comment.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { IMessage } from './message.model';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { CommentsService } from './comments.service';
 import { startWith, Subject, switchMap } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-comment',
@@ -16,9 +17,14 @@ export class CommentComponent {
   newEntry: IMessage;
   selectedEntry: IMessage | null = null;
   private refreshGustMessages$ = new Subject<void>();
-  guestMessages$ = this.refreshGustMessages$.pipe(
-    startWith(null), // so it loads initially
-    switchMap(() => this.service.guestMessages$) // <-- call a method that returns Observable<IMessage[]>
+
+  // Convert observable to signal
+  readonly guestMessagesSignal = toSignal(
+    this.refreshGustMessages$.pipe(
+      startWith(null),
+      switchMap(() => this.service.guestMessages$)
+    ),
+    { initialValue: [] as IMessage[] }
   );
 
   constructor(private service: CommentsService) {
@@ -37,12 +43,8 @@ export class CommentComponent {
     if (this.newEntry.name && this.newEntry.message && this.newEntry.email) {
       this.newEntry.date = new Date();
       this.service.postMessage(this.newEntry).subscribe(entry => {
-        // Optionally, you can handle the response here 
-        console.log('Entry added:', entry);
-        // Refresh the guest messages list
         this.refreshGustMessages$.next();
       });
-      // Reset the form
       this.newEntry = {
         id: 0,
         name: '',
@@ -54,5 +56,4 @@ export class CommentComponent {
       };
     }
   }
-
 }
