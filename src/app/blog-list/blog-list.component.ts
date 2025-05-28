4623ba87ea542c8c5e85c061417d4a125f4e449e
Ievgien <ievgien.iaroshenko@ctco.lv>
2025-05-28 16:01:32 +0400

    add signals and pagination to blog page

--- src/app/blog-list/blog-list.component.ts
+++ src/app/blog-list/blog-list.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { BlogService } from './blog.service';
 import { IBlog } from './blog.model';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-blog-list',
@@ -12,8 +13,16 @@ import { Router } from '@angular/router';
   styleUrl: './blog-list.component.scss'
 })
 export class BlogListComponent {
-  readonly blogs$ = this.blogService.blogs$;
-  constructor(private blogService: BlogService, private router: Router) { }
+  readonly pageSize = 6;
+  readonly blogsSignal = toSignal(this.blogService.blogs$, { initialValue: [] as IBlog[] });
+
+  currentPage = signal(1);
+
+  readonly totalPages = computed(() =>
+    Math.ceil(this.blogsSignal().length / this.pageSize) || 1
+  );
+
+  constructor(private blogService: BlogService, private router: Router) {}
 
   onBlogSelected(blog: IBlog) {
     this.router.navigate(['/blogs', blog.id], { state: { blog } });
