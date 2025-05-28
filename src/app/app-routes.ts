a53acd15ff1cdc394dfeac44f973cbab3560413d
Ievgien <ievgien.iaroshenko@ctco.lv>
2025-05-28 20:41:26 +0400

    add sign in and register forms

--- src/app/app-routes.ts
+++ src/app/app-routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { BlogListComponent } from './blog-list/blog-list.component';
 import { GuestBookComponent } from './guest-book/guest-book.component';
 import { BlogCommentComponent } from './blog-comment/blog-comment.component';
+import { RegisterComponent } from './register/register.component';
 
 export const appRoutes: Routes = [
   { path: 'blogs', component: BlogListComponent, title: "Home - List of blogs" },
   { path: 'blogs/:id', component: BlogCommentComponent, title: "Blog Details" },
   { path: 'guest-book', component: GuestBookComponent, title: "GuestBook - Welcome to guest book" },
+  { path: 'register', component: RegisterComponent, title: "Register - welcome" },
   { path: '', redirectTo: '/blogs', pathMatch: 'full' },
 ];
--- src/app/site-header/site-header.component.ts
+++ src/app/site-header/site-header.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-site-header',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule, FormsModule],
   templateUrl: './site-header.component.html',
   styleUrl: './site-header.component.scss'
 })
 export class SiteHeaderComponent {
+  
+  showLoginPopup = false;
+  email = '';
+  password = '';
+  readonly isSignedInSignal = signal(!!localStorage.getItem('token'));
+
+  onSignIn() {
+    // Your sign-in logic here
+    this.showLoginPopup = false;
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.isSignedInSignal.set(false);
+    // Optionally, navigate or reload
+  }
 
 }
