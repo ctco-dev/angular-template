9f8845449762de0a6004644dbbfac53435447402
Ievgien <ievgien.iaroshenko@ctco.lv>
2025-05-28 19:26:01 +0400

    refactor structure of guest book

--- src/app/guest-book/comment-panel/comment/comment.component.ts
+++ src/app/guest-book/comment-panel/comment/comment.component.ts
@@ -1,11 +1,58 @@
 import { Component } from '@angular/core';
+import { IMessage } from './message.model';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { CommentsService } from './comments.service';
+import { startWith, Subject, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-comment',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './comment.component.html',
   styleUrl: './comment.component.scss'
 })
 export class CommentComponent {
+
+  newEntry: IMessage;
+  selectedEntry: IMessage | null = null;
+  private refreshGustMessages$ = new Subject<void>();
+  guestMessages$ = this.refreshGustMessages$.pipe(
+    startWith(null), // so it loads initially
+    switchMap(() => this.service.guestMessages$) // <-- call a method that returns Observable<IMessage[]>
+  );
+
+  constructor(private service: CommentsService) {
+    this.newEntry = {
+      id: 0,
+      name: '',
+      message: '',
+      email: '',
+      avatar: 'user-m.png',
+      gender: 'male',
+      date: new Date()
+    };
+  }
+
+  addEntry() {
+    if (this.newEntry.name && this.newEntry.message && this.newEntry.email) {
+      this.newEntry.date = new Date();
+      this.service.postMessage(this.newEntry).subscribe(entry => {
+        // Optionally, you can handle the response here 
+        console.log('Entry added:', entry);
+        // Refresh the guest messages list
+        this.refreshGustMessages$.next();
+      });
+      // Reset the form
+      this.newEntry = {
+        id: 0,
+        name: '',
+        message: '',
+        email: '',
+        avatar: 'user-m.png',
+        gender: 'male',
+        date: new Date()
+      };
+    }
+  }
 
 }
--- src/app/guest-book/guest-book.component.ts
+++ src/app/guest-book/guest-book.component.ts
@@ -1,58 +1,15 @@
 import { Component } from '@angular/core';
-import { IMessage } from './message.model';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { CommentsService } from './comments.service';
-import { startWith, Subject, switchMap } from 'rxjs';
+import { CommentComponent } from './comment-panel/comment/comment.component';
 
 @Component({
   selector: 'app-guest-book',
-  imports: [FormsModule, CommonModule],
+  imports: [CommentComponent, FormsModule, CommonModule],
   standalone: true,
   templateUrl: './guest-book.component.html',
   styleUrl: './guest-book.component.scss'
 })
 export class GuestBookComponent {
-  newEntry: IMessage;
-  selectedEntry: IMessage | null = null;
-  private refreshGustMessages$ = new Subject<void>();
-  guestMessages$ = this.refreshGustMessages$.pipe(
-    startWith(null), // so it loads initially
-    switchMap(() => this.service.guestMessages$) // <-- call a method that returns Observable<IMessage[]>
-  );
-
-  constructor(private service: CommentsService) {
-    this.newEntry = {
-      id: 0,
-      name: '',
-      message: '',
-      email: '',
-      avatar: 'user-m.png',
-      gender: 'male',
-      date: new Date()
-    };
-  }
-
-  addEntry() {
-    if (this.newEntry.name && this.newEntry.message && this.newEntry.email) {
-      this.newEntry.date = new Date();
-      this.service.postMessage(this.newEntry).subscribe(entry => {
-        // Optionally, you can handle the response here 
-        console.log('Entry added:', entry);
-        // Refresh the guest messages list
-        this.refreshGustMessages$.next();
-      });
-      // Reset the form
-      this.newEntry = {
-        id: 0,
-        name: '',
-        message: '',
-        email: '',
-        avatar: 'user-m.png',
-        gender: 'male',
-        date: new Date()
-      };
-    }
-  }
 
 }
