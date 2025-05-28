6a0ec42bdd24e6654c94e68757532bc8c7277654
Ievgien <ievgien.iaroshenko@ctco.lv>
2025-05-28 16:44:12 +0400

    add sort by on blog panel

--- src/app/blog-list/blog-list.component.ts
+++ src/app/blog-list/blog-list.component.ts
@@ -4,10 +4,11 @@ import { IBlog } from './blog.model';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-blog-list',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   standalone: true,
   templateUrl: './blog-list.component.html',
   styleUrl: './blog-list.component.scss'
@@ -17,6 +18,17 @@ export class BlogListComponent {
   readonly blogsSignal = toSignal(this.blogService.blogs$, { initialValue: [] as IBlog[] });
 
   currentPage = signal(1);
+
+  orderBy = signal<'asc' | 'desc'>('desc');
+
+  sortedBlogs = computed(() => {
+    const blogs = this.blogsSignal().slice();
+    return blogs.sort((a, b) => {
+      const dateA = new Date(a.date).getTime();
+      const dateB = new Date(b.date).getTime();
+      return this.orderBy() === 'asc' ? dateA - dateB : dateB - dateA;
+    });
+  });
 
   readonly totalPages = computed(() =>
     Math.ceil(this.blogsSignal().length / this.pageSize) || 1
