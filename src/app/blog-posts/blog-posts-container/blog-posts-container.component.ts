8811d9c851846cfd1204569194751b22a8d31d31
Igor Gursky <igor.gursky@ctco.lv>
2025-05-29 18:01:40 +0300

    blogposts: comments page main logic

--- src/app/blog-posts/blog-posts-container/blog-posts-container.component.ts
+++ src/app/blog-posts/blog-posts-container/blog-posts-container.component.ts
@@ -1,15 +1,16 @@
 import {Component, Input} from '@angular/core';
 import {IBlogPost} from "../blog-posts.model";
-import {MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardTitle, MatCardTitleGroup} from "@angular/material/card";
+import {MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardSubtitle, MatCardTitle, MatCardTitleGroup} from "@angular/material/card";
 import {MatButton} from "@angular/material/button";
 import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-blog-posts-container',
-  imports: [MatCard, MatCardHeader, MatCardTitle, MatCardTitleGroup, MatCardContent, MatCardActions, MatButton, RouterLink],
+  imports: [MatCard, MatCardHeader, MatCardTitle, MatCardTitleGroup, MatCardContent, MatCardActions, MatButton, RouterLink, MatCardSubtitle],
   templateUrl: './blog-posts-container.component.html',
   styleUrl: './blog-posts-container.component.scss'
 })
 export class BlogPostsContainerComponent {
   @Input() blogpost!: IBlogPost;
+  @Input() expandedMode!: boolean;
 }
--- src/app/blog-posts/blog-posts.selectors.ts
+++ src/app/blog-posts/blog-posts.selectors.ts
@@ -9,5 +9,5 @@ const selectBlogPosts = createFeatureSelector<store.BlogPostState>(
 export const selectAllBlogPosts = createSelector(selectBlogPosts, (state) => state.loadedPosts);
 
 export const selectBlogPostById = createSelector(selectBlogPosts, (state: BlogPostState, props: { blogPostId: number }) => {
-  return state.loadedPosts.filter(post => post.id === props.blogPostId).pop();
+  return state.loadedPosts.filter(post => post.id == props.blogPostId).pop();
 });
