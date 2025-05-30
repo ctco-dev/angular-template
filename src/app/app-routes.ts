be482c2983b99cdb929e248549943239f975445d
dacezarina <zarina.dace@yahoo.com>
2025-05-30 14:24:30 +0300

    Added loadComponents for faster routing loading

--- src/app/app-routes.ts
+++ src/app/app-routes.ts
@@ -1,11 +1,8 @@
 import { Routes } from '@angular/router';
-import { GuestBookComponent } from './features/guest-page/guest-book/guest-book.component';
-import { PostsPageComponent } from './features/blog-posts/posts-page/posts-page.component';
-import { PostDetailsPageComponent } from './features/blog-posts/post-details-page/post-details-page.component';
 
 export const appRoutes: Routes = [
   { path: '', redirectTo: '/blog-posts', pathMatch: 'full' },
-  { path: 'blog-posts', component: PostsPageComponent },
-  { path: 'blog-posts/:id', component: PostDetailsPageComponent },
-  { path: 'guest-book', component: GuestBookComponent }
+  { path: 'blog-posts', loadComponent: () => import('./features/blog-posts/posts-page/posts-page.component').then(m => m.PostsPageComponent) },
+  { path: 'blog-posts/:id', loadComponent: () => import('./features/blog-posts/posts-page/posts-page.component').then(m => m.PostsPageComponent) },
+  { path: 'guest-book', loadComponent: () => import('./features/guest-page/guest-book/guest-book.component').then(m => m.GuestBookComponent) }
 ];
