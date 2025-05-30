163f4be1913747b024f73da653226e1222e9b9ff
Ievgien <ievgien.iaroshenko@ctco.lv>
2025-05-30 16:43:01 +0400

    code review comments. Move login popup to component

--- src/app/app-routes.ts
+++ src/app/app-routes.ts
@@ -4,12 +4,14 @@ import { GuestBookComponent } from './guest-book/guest-book.component';
 import { BlogCommentComponent } from './blog-comment/blog-comment.component';
 import { RegisterComponent } from './register/register.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { LoginComponent } from './login/login.component';
 
 export const appRoutes: Routes = [
   { path: 'blogs', component: BlogListComponent, title: "Home - List of blogs" },
   { path: 'blogs/:id', component: BlogCommentComponent, title: "Blog Details" },
   { path: 'guest-book', component: GuestBookComponent, title: "GuestBook - Welcome to guest book" },
   { path: 'register', component: RegisterComponent, title: "Register - welcome" },
+  { path: 'login', component: LoginComponent, title: "Login - welcome" },
   { path: '', redirectTo: '/blogs', pathMatch: 'full' },
   { path: '**', component: NotFoundComponent, title: "Page Not Found" } 
 ];
