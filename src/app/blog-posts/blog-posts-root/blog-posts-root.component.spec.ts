c0fae1f422c3ea43d99a0da3186db37778374c22
Igor Gursky <igor.gursky@ctco.lv>
2025-05-30 17:38:55 +0300

    testing: component test fix

--- src/app/blog-posts/blog-posts-root/blog-posts-root.component.spec.ts
+++ src/app/blog-posts/blog-posts-root/blog-posts-root.component.spec.ts
@@ -1,21 +1,28 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {BlogPostsRootComponent} from './blog-posts-root.component';
-import {provideMockStore} from "@ngrx/store/testing";
+import {provideMockStore, MockStore} from '@ngrx/store/testing';
+import {selectAllBlogPosts} from '../blog-posts.selectors';
 
 describe('BlogPostsRootComponent', () => {
   let component: BlogPostsRootComponent;
   let fixture: ComponentFixture<BlogPostsRootComponent>;
+  let store: MockStore;
 
   beforeEach(async () => {
-    TestBed.configureTestingModule({
+    await TestBed.configureTestingModule({
+      imports: [BlogPostsRootComponent],
       providers: [
         provideMockStore({
-          initialState: {
-            loadedPosts: []
+            initialState: {
+              loadedPosts: []
+            }
           }
-        })
+        )
       ]
-    })
+    }).compileComponents();
+
+    store = TestBed.inject(MockStore);
+    store.overrideSelector(selectAllBlogPosts, []);
 
     fixture = TestBed.createComponent(BlogPostsRootComponent);
     component = fixture.componentInstance;
