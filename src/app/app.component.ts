664f54f6dde39bb0df6e9b076370ca0d6c19b87d
Vitaly Koritko <vitaly.koritko@ctco.lv>
2025-11-26 10:15:47 +0200

    first task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppCardComponent } from "./components/app-card/app-card.component";
+import { Article } from './components/app-card/models/article';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppCardComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-template';
+  article: Article = {
+    title: 'Shiba Inu',
+    description: 'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.',
+    picture: 'https://material.angular.dev/assets/img/examples/shiba2.jpg',
+    publishDate: new Date('2024-11-21'),
+  };
 }
